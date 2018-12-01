c957d423b771aa6c1b5e10b27b7a2718f0ab940b
Mahmoud Alsaeed <m.alsaeed97.ma@gmail.com>
2018-11-30 22:26:38 -0500

    Add files via upload

---
+++ ResidentialAnalysis.py
@@ -0,0 +1,64 @@
+from pyspark import SparkConf,SparkContext
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
+from dateutil.parser import parse
+from operator import add
+import pyspark
+import sys
+import requests
+
+
+conf = SparkConf()
+conf.setAppName("Taxi_vs_Uber")
+# create spark context with the above configuration
+sc = SparkContext(conf=conf)
+sc.setLogLevel("ERROR")
+# Create an sql context so that we can query data files in sql like syntax
+sqlContext = SQLContext (sc)
+# read the csv file
+uberFile = sc.textFile("file:///app/uber_clean_sample.csv")
+taxiFile = sc.textFile("file:///app/taxi_clean_sample.csv")
+
+# remove the header from UberFile
+uberHeader = uberFile.filter(lambda line: "Pickup_time" in line)
+uberFileWithNoHeader = uberFile.subtract(uberHeader)
+
+#------------I know that his code is somehow contains duplicate code, but for readablity purposes I will keep it as it is
+
+# remove the header from Taxi File
+taxiHeader = taxiFile.filter(lambda line: "Pickup_time" in line)
+taxiFileWithNoHeader = taxiFile.subtract(taxiHeader)
+
+
+#------ Note: we're mapping the pickup date to 2 because we know that the data we're parsing is like only 50% of all uber rides in NYC.
+
+# takes a triplet in this format (pickup_date,pickup_time,borough) and returns a tuple in the following format
+#((pickup_date,borough), value) where the pickup date only contain the year and the month
+def mapper(x,value):
+	# parse the date
+	date = parse(x[0])
+	# extract the year and the month and map them to 2
+	return((str(date.year) + "-" + str(date.month),x[2]),value)
+
+# for each line in the Uber file, this line will map the pickup_date to tuple, check the mapper function for details about the tuple.
+uber_temp = uberFileWithNoHeader.map(lambda line: line.split(",")).map(lambda x : mapper(x,2))
+taxi_temp = taxiFileWithNoHeader.map(lambda line: line.split(",")).map(lambda x : mapper(x,1))
+
+# reduce by key to get the total number of rides in NYC in a month
+uber_temp= uber_temp.reduceByKey(add)
+taxi_temp= taxi_temp.reduceByKey(add)
+
+# remove this line before submitting the code.
+print("Number of uber rides per month per zone")
+print(uber_temp.collect())
+
+print("Number of taxi rides per month per zone")
+print(taxi_temp.collect())
+
+
+
+
+
+
+
+ 
